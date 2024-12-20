40f6a539642808bfd073280a3c2e15eb387af078
Alangithub12 <alanakhmedov@MacBook-Air-Alan.local>
2024-12-20 21:31:30 +0300

    Создан класс ShoeView для отображения информации об обуви: - Реализованы методы для отображения списка обуви и сообщений.

---
+++ shoe_view.py
@@ -0,0 +1,9 @@
+class ShoeView:
+    @staticmethod
+    def display_shoes(shoes):
+        for shoe in shoes:
+            print(shoe)
+
+    @staticmethod
+    def display_message(message):
+        print(message)
